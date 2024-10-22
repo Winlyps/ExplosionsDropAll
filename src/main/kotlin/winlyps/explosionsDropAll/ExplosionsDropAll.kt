9adf928b75f50c7193158e335b8e11fce3e5a8c8
Winlypse <nts3@o2.pl>
2024-10-22 13:43:10 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/explosionsDropAll/ExplosionsDropAll.kt
@@ -0,0 +1,14 @@
+package winlyps.explosionsDropAll
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ExplosionsDropAll : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
